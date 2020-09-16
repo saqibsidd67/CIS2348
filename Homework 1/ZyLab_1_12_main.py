631898c0728d09eff6377c2cb5766959f0f781f4
saqibsidd67 <70624776+saqibsidd67@users.noreply.github.com>
2020-09-16 11:08:59 -0500

    Add files via upload
    ZyLab 1.12 HW 1

---
+++ Homework 1/ZyLab_1_12_main.py
@@ -0,0 +1,7 @@
+# Saqib Siddiqui
+# PSID: 1495537
+
+userNum = int(input())
+userNumSquared = userNum * userNum # Bug here; fix it when instructed
+
+print(userNumSquared, end = '\n') # Output formatting issue here; fix it when instructed
