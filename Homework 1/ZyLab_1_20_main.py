a05652e7c0620593e4fe8a281aeacdc4a37f868b
saqibsidd67 <70624776+saqibsidd67@users.noreply.github.com>
2020-09-16 11:17:54 -0500

    Add files via upload
    ZyLab 1.20 HW 1

---
+++ Homework 1/ZyLab_1_20_main.py
@@ -0,0 +1,18 @@
+# Saqib Siddiqui
+# PSID:1495537
+
+print('Enter integer:')
+user_num1 = int(input())
+print('You entered:',user_num1)
+num1sq = user_num1 * user_num1
+num1cub = user_num1 * user_num1 * user_num1
+print(user_num1,'squared is',num1sq)
+print('And',user_num1,'cubed is',num1cub,'!!')
+print('Enter another integer:')
+user_num2 = int(input())
+num_sum = user_num1 + user_num2
+num_product = user_num1 * user_num2
+print(user_num1,'+',user_num2,'is',num_sum)
+print(user_num1,'*',user_num2,'is',num_product)
+
+
