4ccc305b0c5abbd52dbe5aa7b89f07df68163988
saqibsidd67 <70624776+saqibsidd67@users.noreply.github.com>
2020-10-05 23:43:42 -0500

    Create readme.py
    Homework 2  Lab Content

---
+++ Homework 2/readme.py
@@ -0,0 +1,1 @@
+The content in the Homework 2 folder contains the codes for Coding Problem 2, Zylab Challenge 6.17, Zylab Challenge 6.22, Zylab Challenge 7.25, Zylab Challenge 8.10,and Zylab Challenge 9.10.
